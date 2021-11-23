905c0830c845e335e83022e8116b0056e05bbcb2
Michał Płużek <michalpluzek@gmail.com>
2021-11-23 16:08:32 +0100

    Add better display users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const UsersList = (props) => {
-  const users = props.users.map((user) => <li>{user.name.first}</li>);
+  const users = props.users.map((user) => (
+    <div key={user.login.uuid}>
+      <h4>{`${user.name.first} ${user.name.last}`}</h4>
+      <p>{user.email}</p>
+    </div>
+  ));
 
   return <ul>{users}</ul>;
 };
